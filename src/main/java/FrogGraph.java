0482d041a13d4b58c9db4d2e0dc8f8c029f3a76c
miaccbelli <111941171+miaccbelli@users.noreply.github.com>
2024-05-06 10:56:30 -0700

    finished createGraph() and printSolutions() - need to debug both still

--- src/main/java/FrogGraph.java
+++ src/main/java/FrogGraph.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Array;
 import java.util.*;
 
 public class FrogGraph {
@@ -80,50 +81,96 @@ public class FrogGraph {
          * predecessorMap, frogNeighbors, distFromStarting (if used), and winningArrangements
          */
     }
-    protected void createGraph(FrogArrangement frog){ // remember! this is a BFS search method
+
+    /***
+     * createGraph(): uses a BFS search method to explore all possible frog arrangements from the
+     * starting arrangement, and using the predecessor map, it keeps track of which arrangement came
+     * before each new arrangement, frog neighbors tells us which frog arrangements are
+     * connected/neighbors, and we call isWinningArrangement from the frogArrangement class to
+     * determine if there is 1 frog left and adds this new arrangement to the winningArrangements queue
+     *
+     * @param frog - the starting frog arrangement
+     */
+    protected void createGraph(FrogArrangement frog) {
         FrogArrangement curFrogs = startingArrangement;
         Queue<FrogArrangement> curQueue = new ArrayDeque<>();
         frogNeighbors.put(curFrogs, curQueue); // adding currFrogs into frog neighbors
         Queue<FrogArrangement> bfsQueue = new ArrayDeque<>();
         bfsQueue.add(curFrogs); // all setting us up to do a bfs
 
-        while(!bfsQueue.isEmpty()) {
+        while (!bfsQueue.isEmpty()) {
             FrogArrangement current = bfsQueue.poll(); // removing form queue
 
+            // iterating over all possible hops in the curr frogArrangement
             for (int[] from : FrogArrangement.hopOptions.keySet()) {
                 for (int[] over : FrogArrangement.hopOptions.get(from).keySet()) {
                     int[] to = FrogArrangement.hopOptions.get(from).get(over);
+
                     // can we do that hop on curFrogs? (call canHop() method from FrogArrangement)
-                    // if yes (it can hop): create newFrogs by doing the hop
-                    // if newFrogs has not been seen before (aka must check if newFrogs is a key in the predecessor Map)
-                    // add it to bfsQueue
-                    // add curFrogs as its predecessor
-                    // check to see if it is a winning state (if so, add to winning Arrangements)
-                    // create Queue in the neighborMap, corresponding to key newFrogs
-                    // add newFrogs as neighbor of curFrogs
-                    // add curFrogs as neighbor of newFrogs
+                    if (current.canHop(from, over, to)) {
+                        FrogArrangement newFrogs = hop(current, from, over, to); // making new arrangement to show hop
+
+                        if (!predecessorMap.containsKey(newFrogs)) { // if newFrogs has not been seen before (must check if newFrogs is a key in the predecessor Map)
+                            bfsQueue.add(newFrogs); // add new frogs to bfsQueue
+                            predecessorMap.put(newFrogs, current); // mark predecessor
+                            if (FrogArrangement.isWinningState(newFrogs)) {
+                                winningArrangements.offer(newFrogs); // adding newFrogs to the winnningArrangement queue
+                            }
+                            frogNeighbors.put(newFrogs, new ArrayDeque<>()); // making new Queue in the neighborMap, corresponding to key newFrogs
+                        }
+                        // add newFrogs as neighbor of curFrogs
+                        frogNeighbors.get(current).offer(newFrogs);
+                        // add curFrogs as neighbor of newFrogs
+                        frogNeighbors.get(newFrogs).offer(current);
+                    }
                 }
             }
         }
+    }
 
-        /***
-         * TODO: implement this method.
-         * The FrogArrangement frog is the startingArrangement.
-         * This method must populate all of :
-         * predecessorMap, frogNeighbors, and winningArrangements
-         * It will also populate distFromStarting if you decide to use it
-         */
-    }
+    /***
+     * printSolutions(): prints all possible solutions and iterates through the winning arrangements
+     * to print all the steps to reach a winning arrangement from the starting arrangement (prints
+     * using a stack where the moves are in reverse order (LIFO))
+     */
     public void printSolutions(){
-        /***
-         * TODO: implement this method.
-         * See the project description on Canvas to see how the print should look.
-         * MIGHT WANT TO USE A STACK TO PRINT THE STEPS/MOVES BACKWARDS FROM THE WINNING ARRANGEMENT TO
-         * THE STARTING ARRANGEMENT
-         * USING THE PREDECESSOR OF THE WINNING ARRANGEMENT TO THE PREVIOUS STEP AND THEN ITS PREVIOUS STEP, ETC
-         * PRINT FROM THE STACK (LIFO)
-         */
+        if(winningArrangements.isEmpty()) { // if there are no winning arrangements
+            System.out.println("No winning arrangements found.");
+            return;
+        }
+
+        Deque<String> movesStack = new ArrayDeque<>(); // creating a stack to store & print the moves/steps
+        System.out.println("Number of different ending states: " + winningArrangements.size() +".");
+
+        for(FrogArrangement winningArrangement : winningArrangements) {
+            System.out.println("This arrangement: ");
+            System.out.println(winningArrangement);
+
+            int distance = distFromStarting.get(winningArrangement);
+            System.out.println("Has distance " + distance + " from the starting position.");
+
+            // getting all the moves from the predecessor map and putting into our stack
+            FrogArrangement curArrangement = winningArrangement;
+            while(predecessorMap.containsKey(curArrangement)) {
+                FrogArrangement predecessor = predecessorMap.get(curArrangement);
+                List<String> moves = getMoves(predecessor, curArrangement);
+                for(String move : moves) {
+                    movesStack.push(move);
+                }
+                curArrangement = predecessor;
+            }
+
+            while(!movesStack.isEmpty()) {
+                // printing the top of stack and popping it off to move to the move (producing LIFO system)
+                System.out.println(movesStack.pop());
+            }
+
+            System.out.println("----------");
+
+        }
+
     }
+
     public boolean areAdjacent(FrogArrangement frogs1, FrogArrangement frogs2){
         /***
          * TODO: implement this method.
@@ -136,7 +183,8 @@ public class FrogGraph {
          */
         return false;
     }
-    public static ArrayDeque<Integer> loadFrogs(String frogFilename){
+
+    public static ArrayDeque<Integer> loadFrogs(String frogFilename) {
         /***
          * TODO: Describe this method with javadoc style documentation, but don't change
          * This method is just here so that you can do additional debugging checks.
@@ -176,6 +224,7 @@ public class FrogGraph {
             return numOfSolutionsArray;
         }
     }
+
     private static int intValue(String s){
         /***
          * TODO: do not change this method, but explain what this method does using javadoc style documentation
@@ -188,3 +237,4 @@ public class FrogGraph {
         }
     }
 }
+
